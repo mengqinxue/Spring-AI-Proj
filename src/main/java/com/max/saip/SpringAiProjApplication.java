09a1a6332ed42379034a17eb5f27e85e08d7bf69
Max <Qinxue.Meng@gmail.com>
2025-05-05 09:28:51 +0800

    Init

---
+++ src/main/java/com/max/saip/SpringAiProjApplication.java
@@ -0,0 +1,13 @@
+package com.max.saip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiProjApplication.class, args);
+	}
+
+}
