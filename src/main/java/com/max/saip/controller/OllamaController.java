ae5ede9a180786f493378aa0b69f0f58129ce537
Max <Qinxue.Meng@gmail.com>
2025-05-08 13:50:27 +0800

    Init

--- src/main/java/com/max/saip/controller/OllamaController.java
+++ src/main/java/com/max/saip/controller/OllamaController.java
@@ -12,10 +12,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 @RestController
 @CrossOrigin
@@ -27,11 +30,15 @@ public class OllamaController {
         this.chatClient = chatClientBuilder.build();
     }
 
-    @Autowired
-    ChatClient.Builder chatClientBuilder;
+    /**
+     * Chat with one-time resposne.
+     * @param message
+     * @return
+     * Example:  http://localhost:8080/api/ollama/chat?message=hello
+     */
     @CrossOrigin
     @GetMapping(value = "/api/ollama/chat")
-    public String chat(@RequestParam(value = "message", defaultValue = "讲个笑话") String message) {
+    public String chat(@RequestParam(value = "message", defaultValue = "tell me a story") String message) {
 
         return chatClient.prompt()
                 .system(s -> s.param("current_date", LocalDate.now().toString()))
@@ -40,5 +47,38 @@ public class OllamaController {
                 .content();
 
     }
+
+    /**
+     * Chat with streaming resposne.
+     * @param message
+     * @return
+     * Example:  http://localhost:8080/api/ollama/stream_chat
+     */
+    @CrossOrigin
+    @GetMapping(value = "/api/ollama/stream_chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> generateStreamAsString(@RequestParam(value = "message", defaultValue = "tell me a joke") String message) {
+        Flux<String> content = chatClient.prompt()
+                .system(s -> s.param("current_date", LocalDate.now().toString()))
+                //.advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId).param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+                .advisors(a -> a.param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+                .user(message)
+                .stream()
+                .content();
+
+        return  content
+                .concatWith(Flux.just("[complete]"));
+
+    }
+
+
+    @CrossOrigin
+    @GetMapping(value = "/api/ollama/mcp")
+    public String mcpCall(@RequestParam(value = "message", defaultValue = "hello") String message) {
+
+
+        return message;
+    }
+
+
 
 }
---
+++ src/main/java/com/max/saip/service/BookingTools.java
@@ -0,0 +1,11 @@
+package com.max.saip.service;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BookingTools {
+
+
+
+
+}
